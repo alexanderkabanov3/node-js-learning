e5a003c0c8c341cef472c798945e343e070b919a
Alexander <sanexwell@bk.ru>
2021-09-02 08:59:09 +0300

    templates was added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -17,18 +20,32 @@ const tempCard = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-  // overview page
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(tempOverview);
+const slugs = dataObj.map((el) => slugify(el.productName, {lower: true}));
 
-  if (req.url === '/' || req.url === '/overview') {
+const server = http.createServer((req, res) => {
+  const {query, pathname} = url.parse(req.url, true);
+
+  // overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+
+    res.end(output);
+
     //products page
-  } else if (req.url === '/products') {
-    res.end('You are on products page');
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const product = dataObj[query.id];
+
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (req.url === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {'Content-type': 'application/json'});
     res.end(data);
 
